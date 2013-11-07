ac337e10e65a93526c52e5dbf0d482e43079f911
root <root@v28388581sl0001.defrft6.savvis.net>
2013-11-07 08:06:30 +0000

    adding working set of files pre changes to template name and more logging output

--- vcloud-client.py
+++ vcloud-client.py
@@ -13,29 +13,32 @@ vCloud_Logger.filename = sys.argv[1]
 
 # set api credentials and endpoint
 
-username 	= 'oliver.api'
-org      	= 'SymphonyVPDCDemo2'
-password 	= 'A12345'
-key      	= 'l7xxd3a1965751154a0fae7373bd79628ffb'
-secret   	= '5a8eeb6ef18947bea5451130d5a35012'
-endpoint 	= 'https://api.savvis.net/clouddatacenter/vcloud/de-central-1/'
+username 	= ''
+org      	= ''
+password 	= ''
+key      	= ''
+secret   	= ''
+endpoint 	= ''
 
 # set servername for virtual machine
 
-z.log(lvl='c',msg=("setting servername for vCloud virtual machine"))
+z.log(lvl='i',msg=("setting servername for vCloud virtual machine"))
 y.get_servername()
 
 # login to vCloud 
 
+z.log(lvl='i',msg=("logging in to vcloud to get session headers"))
 x.sessions(username, org, password, key, secret, endpoint)
 
 # Deploy VM and with template values - puppet-win-2008r2 or puppet-centos-6x
 
+z.log(lvl='i',msg=("deploying virtual machines from vapp template"))
 x.vapp_templates(template_name = 'puppet-centos-6x')
 x.vapps(vapp_name = 'ol-vapp-04')
 
 # insert the virtual machine details in to the vCloud vm_instance database 
 
+z.log(lvl='i',msg=("updating virtual machine name"))
 y.update_server(vm_name=vCloud_Lookup.vm_name, vm_href=vCloud_Lookup.vm_href, vm_ip=vCloud_Lookup.vm_ip)
 
 # return exit value 
